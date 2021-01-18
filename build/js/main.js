009f1ae25a607e6575e3f35e59b49fc3e97dfc2a
hudson8811 <hudson88@yandex.ru>
2021-01-18 19:44:11 +0300

    chanegd

--- build/js/main.js
+++ build/js/main.js
@@ -15,3 +15,8 @@ $('a.next-link').click(function(e) {
     }, 400);
     return false;
 });
+
+if($(window).width() < 769){
+    $('.revealator-slideleft').removeClass('revealator-slideleft revealator-once revealator-delay3');
+    $('.revealator-slideright').removeClass('revealator-slideright revealator-once revealator-delay3');
+}
