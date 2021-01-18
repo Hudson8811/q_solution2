6e20bcf258cf8ad275ceeeabd523d782e1476cf9
hudson8811 <hudson88@yandex.ru>
2021-01-18 20:21:47 +0300

    chanegd

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,13 +1,21 @@
-var $page = $('html, body');
-$('a.next-link').click(function(e) {
-    e.preventDefault();
-    $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 400);
-    return false;
-});
+$(document).ready(function () {
 
-if($(window).width() < 769){
-    $('.revealator-slideleft').removeClass('revealator-slideleft revealator-once revealator-delay3');
-    $('.revealator-slideright').removeClass('revealator-slideright revealator-once revealator-delay3');
-}
+    var $page = $('html, body');
+    $('a.next-link').click(function(e) {
+        e.preventDefault();
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
+    });
+
+    if($(window).width() < 769){
+        $('.revealator-slideleft').removeClass('revealator-slideleft revealator-once revealator-delay3');
+        $('.revealator-slideright').removeClass('revealator-slideright revealator-once revealator-delay3');
+    }
+
+
+    $("img.lazy").lazyload({
+        effect : "fadeIn"
+    });
+  });
